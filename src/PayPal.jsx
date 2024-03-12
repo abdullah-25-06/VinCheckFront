12d676c4eb51ab37a533e6f941dd52b478f74f30
abdullah <abdullahmoudood35@gmail.com>
2024-03-13 04:48:08 +0500

    paypal fixed

--- src/PayPal.jsx
+++ src/PayPal.jsx
@@ -76,90 +76,89 @@ function Paypal(props) {
     return data;
   };
   const scroll = () => {
-
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-
-    
-  }
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   return (
-    
-      <>
-            <a id="topbutton" onClick={scroll}></a>
+    <>
+      <a id="topbutton" onClick={scroll} href=" ">
+        {""}
+      </a>
 
-        <div className=" login">
-          <div className="navhead">
-            <div className="title">
+      <div className=" login">
+        <div className="navhead">
+          <div className="title">
             <NavLink
-            className="navbar-brand"
-            id="brandname"
-            to="/"
-            data-aos="flip-left"
-            data-aos-anchor="#example-anchor"
-            data-aos-offset="500"
-            data-aos-duration="500"
-            data-aos-delay="500"
-          >
-            <img src="wlogo.png" alt="" id="wlogo" style={{marginLeft:'10px'}} />
-          </NavLink>
-            </div>
-            {/* <NavLink to="/Login" className="nav-link"> */}
-              <div className="log">
-                <a href="/Dashboard" style={{textDecoration:'none'}}>
-
-                <p style={{ color: "white" ,marginRight:'6px' }}>Dashboard</p>
-                </a>
-              </div>
-            {/* </NavLink> */}
+              className="navbar-brand"
+              id="brandname"
+              to="/"
+              data-aos="flip-left"
+              data-aos-anchor="#example-anchor"
+              data-aos-offset="500"
+              data-aos-duration="500"
+              data-aos-delay="500"
+            >
+              <img
+                src="wlogo.png"
+                alt=""
+                id="wlogo"
+                style={{ marginLeft: "10px" }}
+              />
+            </NavLink>
           </div>
-          <div className="nborder"></div>
+          {/* <NavLink to="/Login" className="nav-link"> */}
+          <div className="log">
+            <a href="/Dashboard" style={{ textDecoration: "none" }}>
+              <p style={{ color: "white", marginRight: "6px" }}>Dashboard</p>
+            </a>
+          </div>
+          {/* </NavLink> */}
+        </div>
+        <div className="nborder"></div>
 
-          <div className=" logform2 mt-1">
-            <div className="formbox2">
-              <form className="fform">
-                <h3 className="pt-2">Checkout</h3>
+        <div className=" logform2 mt-1">
+          <div className="formbox2">
+            <form className="fform">
+              <h3 className="pt-2">Checkout</h3>
+              <hr />
+              <div className="formbody2">
+                <div className="ftitle">
+                  <p className="frs">Username</p>
+                  <p className="fls">{ctx.username}</p>
+                </div>
+                <div className="ftitle">
+                  <p className="frs">Category</p>
+                  <p className="fls">{title}</p>
+                </div>
+                <div className="ftitle">
+                  <p className="frs">Total Reports</p>
+                  <p className="fls">{report}</p>
+                </div>
+                <div className="ftitle">
+                  <p className="frs">Currency</p>
+                  <p className="fls">{currency}</p>
+                </div>
                 <hr />
-                <div className="formbody2">
-                  <div className="ftitle">
-                    <p className="frs">Username</p>
-                    <p className="fls">{ctx.username}</p>
-                  </div>
-                  <div className="ftitle">
-                    <p className="frs">Category</p>
-                    <p className="fls">{title}</p>
-                  </div>
-                  <div className="ftitle">
-                    <p className="frs">Total Reports</p>
-                    <p className="fls">{report}</p>
-                  </div>
-                  <div className="ftitle">
-                    <p className="frs">Currency</p>
-                    <p className="fls">{currency}</p>
-                  </div>
-                  <hr />
-                  <div className="ftitle mt-4">
-                    <p className="frs"> Total </p>
-                    <p className="fls">{price}</p>
-                  </div>
-                  <hr />
+                <div className="ftitle mt-4">
+                  <p className="frs"> Total </p>
+                  <p className="fls">{price}</p>
                 </div>
+                <hr />
+              </div>
 
-                <div className="lbtn2">
-                  <PayPalScriptProvider options={initialOptions}>
-                    <PayPalButtons
-                      createOrder={(data, actions) =>
-                        createOrder(data, actions)
-                      }
-                      onApprove={(data, actions) => onApprove(data, actions)}
-                    />
-                  </PayPalScriptProvider>
-                </div>
-              </form>
-            </div>
+              <div className="lbtn2">
+                <PayPalScriptProvider options={initialOptions}>
+                  <PayPalButtons
+                    createOrder={(data, actions) => createOrder(data, actions)}
+                    onApprove={(data, actions) => onApprove(data, actions)}
+                  />
+                </PayPalScriptProvider>
+              </div>
+            </form>
           </div>
         </div>
-      </>
-    
+      </div>
+    </>
   );
 }
 
